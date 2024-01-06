c7fe896e0cb24348fcf9b0ea1f4bb000ee00a0cb
leadpogrommer <ilyamerzlakov@gmail.com>
2024-01-06 14:07:33 +0700

    Config support

--- client/main.py
+++ client/main.py
@@ -1,10 +1,13 @@
 import struct
+from typing import Any
+
 import serial
 import time
 import sys
 # from evdev import InputDevice, list_devices, ecodes
 import pygame
 from pygame.locals import *
+import json
 import os
 
 CMD_MOVE_TO = 0
@@ -14,9 +17,11 @@ CMD_RESET_POS = 3
 
 float_cmds = {CMD_SET_SPEED, CMD_SET_ACC}
 
-portFile = '/dev/' + next(filter(lambda a: a.startswith('ttyUSB'), os.listdir('/dev')))
+# portFile = '/dev/' + next(filter(lambda a: a.startswith('ttyUSB'), os.listdir('/dev')))
+portFile = sys.argv[1]
 print(portFile)
 port = serial.Serial(portFile, 115200)
+config = json.load(open(sys.argv[2]))
 
 
 def send_command(cmd, motor, arg: int | float):
@@ -29,64 +34,28 @@ def send_command(cmd, motor, arg: int | float):
     port.write(data)
     # while(port.readall())
 
-
-accs = [50, 100, 150, 200]
-
-
-# found_controller = False
-# devices = [InputDevice(fn) for fn in list_devices()]
-# for dev in devices:
-#   if dev.name == 'Wireless Controller':
-#     found_controller = True
-#     break
-# if not found_controller:
-#   print('Connect controller you moron')
-#   exit()
-#
-# for e in dev.read_loop():
-#     if e.type == ecodes.EV_ABS and e.code == 1:
-#         print(e)
-
 pygame.init()
 print(pygame.joystick.get_count())
-joy = pygame.joystick.Joystick(0)
 
-joy.get_axis(1)
+for i in range(pygame.joystick.get_count()):
+    joy = pygame.joystick.Joystick(i)
+    print(joy.get_name())
+    if joy.get_name() == config['controller']:
+        break
+    joy.quit()
 
-axis_steppers = {
-    1: 3,
-    3: 1,
-}
+
+def keys_to_int(d: dict[str, Any]) -> dict[int, Any]:
+    return {int(k): v for k, v in d.items()}
+
+
+axis_steppers = keys_to_int(config['axis_steppers'])
+axis_config = keys_to_int(config['axis_config'])
+button_steppers = keys_to_int(config['button_steppers'])
+button_config = keys_to_int(config['button_config'])
+
 
 prev_axis_val = {k: None for k in axis_steppers}
-
-button_steppers = {
-    0: 0,
-    1: 2
-}
-
-# button_pressed_coord = 100
-# button_released_coord = 0
-
-# axis_min = 0
-# axis_max = 200*5
-# axis_config = {
-#     3: (0, -1000),
-#     1: (0, 1000),
-# }
-
-axis_config = {
-    3: (0, -280),
-    1: (0, 280),
-}
-
-button_config = {
-    0: (0, 100),
-    1: (1, -100),
-}
-
-
-
 
 
 
@@ -95,17 +64,6 @@ while (cl := port.readline().decode().strip()) != 'READY!':
 print(cl)
 print('Got ready, sending shit')
 
-# for i in range(4):
-#     send_command(CMD_SET_ACC, i, 400)
-#     send_command(CMD_SET_SPEED, i, 3200)
-    # send_command(CMD_MOVE_TO, i, 1000000* (-1**(i+1)))
-
-# while(True):
-#     try:
-#         sys.stdout.write(port.read(1).decode())
-#     except UnicodeDecodeError:
-#         print('X', end='')
-
 
 def map_vals(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -113,20 +71,28 @@ def map_vals(x, in_min, in_max, out_min, out_max):
 
 while True:
     for event in [pygame.event.wait(), ] + pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(0)
         if event.type in (JOYBUTTONUP, JOYBUTTONDOWN):
             btn = event.button
             if btn in button_steppers:
                 dst = button_config[btn][1] if event.type == JOYBUTTONDOWN else button_config[btn][0]
                 send_command(CMD_MOVE_TO, button_steppers[btn], dst)
+            elif btn == config['quit_button']:
+                pygame.quit()
+                exit(0)
             # else:
             #     print('Entering read loop')
             #     while True:
             #         print(port.readline())
         elif event.type == JOYAXISMOTION and (axis := event.axis) in axis_steppers:
             v = event.value
+            # if axis == 1:
+                # print(v)
             if prev_axis_val[axis] is not None and abs(v - prev_axis_val[axis]) < 0.1:
                 continue
             prev_axis_val[axis] = v
+
 
             dst = map_vals(event.value, -1, 1, axis_config[axis][0], axis_config[axis][1])
             send_command(CMD_MOVE_TO, axis_steppers[axis], dst)
